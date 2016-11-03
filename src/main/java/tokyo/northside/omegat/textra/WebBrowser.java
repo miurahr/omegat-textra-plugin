7fd997208fe8d72cc1a3b740800b696cc221bdd1
Hiroshi Miura <miurahr@linux.com>
2016-11-03 11:05:32 +0900

    Not throw exception from WebBrowser class
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/tokyo/northside/omegat/textra/WebBrowser.java
+++ src/main/java/tokyo/northside/omegat/textra/WebBrowser.java
@@ -12,16 +12,18 @@ public final class WebBrowser {
     /**
      * Start default web browser with URL.
      * @param url open page of url.
-     * @throws IOException when fails to start browser.
-     * @throws URISyntaxException when URL is invalid.
      */
-    public static void launch(final String url) throws IOException, URISyntaxException {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            desktop.browse(new URI(url));
-        } else {
-            Runtime runtime = Runtime.getRuntime();
-            runtime.exec("xdg-open " + url);
+    public static void launch(final String url) {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                desktop.browse(new URI(url));
+            } else {
+                Runtime runtime = Runtime.getRuntime();
+                runtime.exec("xdg-open " + url);
+            }
+        } catch (IOException | URISyntaxException ex) {
+            // ignore me.
         }
     }
 
