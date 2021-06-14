f2e401fe880530140b535493ae48e6cd44d22d9e
Hiroshi Miura <miurahr@linux.com>
2021-06-15 07:57:56 +0900

    Bump versions of dependencies
    - signpost@2.1.1
    - jackson@2.12.3
    - commons-io@2.7
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,11 +26,11 @@ configure<JavaPluginConvention> {
 }
 
 dependencies {
-    packIntoJar("oauth.signpost:signpost-core:1.2.1.2")
-    packIntoJar("oauth.signpost:signpost-commonshttp4:1.2.1.2")
-    packIntoJar("org.apache.httpcomponents:httpclient:4.5.13")
-    packIntoJar("com.fasterxml.jackson.core:jackson-core:2.12.0")
-    packIntoJar("com.fasterxml.jackson.core:jackson-databind:2.12.0")
+    packIntoJar("oauth.signpost:signpost-core:2.1.1")
+    packIntoJar("oauth.signpost:signpost-commonshttp4:2.1.1")
+    packIntoJar("org.apache.httpcomponents:httpclient:4.5.12")
+    packIntoJar("com.fasterxml.jackson.core:jackson-core:2.12.3")
+    packIntoJar("com.fasterxml.jackson.core:jackson-databind:2.12.3")
     packIntoJar(fileTree("lib") {include("*.jar")})
     packIntoJar("org.slf4j:slf4j-api:1.7.25")
     implementation("commons-io:commons-io:2.5")
