d647cb087a39daa857a6f2817ab2db344c4245e1
Hiroshi Miura <miurahr@linux.com>
2023-02-04 08:48:07 +0900

    chore(gradle): Bump junit@5.9.8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     testImplementation("commons-io:commons-io:2.11.0")
     testImplementation("commons-lang:commons-lang:2.6")
     testImplementation("org.codehaus.groovy:groovy-all:3.0.12")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 repositories {
     mavenCentral()
